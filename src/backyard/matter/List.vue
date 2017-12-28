5aca8292b1ce7e3980277b9f483853c2c262b156
seaheart <ycyin8795@163.com>
2017-12-28 15:09:04 +0800

    complete folder move batch.

--- src/backyard/matter/List.vue
+++ src/backyard/matter/List.vue
@@ -7,17 +7,25 @@
 				<div>
 					<NbFilter :pager="pager" :callback="search">
 
-						<button class="btn btn-primary btn-sm" v-if="temporaryMatterUuids.length !== pager.data.length" @click.stop.prevent="checkAll">
+						<button class="btn btn-primary btn-sm" v-if="temporaryMatterUuids.length !== pager.data.length"
+						        @click.stop.prevent="checkAll">
 							<i class="fa fa-check-square"></i>
 							全选
 						</button>
-						<button class="btn btn-primary btn-sm" v-if="pager.data.length && temporaryMatterUuids.length === pager.data.length" @click.stop.prevent="checkNone">
+						<button class="btn btn-primary btn-sm"
+						        v-if="pager.data.length && temporaryMatterUuids.length === pager.data.length"
+						        @click.stop.prevent="checkNone">
 							<i class="fa fa-square-o"></i>
 							取消全选
 						</button>
 						<button class="btn btn-primary btn-sm" v-if="temporaryMatterUuids.length" @click.stop.prevent="deleteBatch">
 							<i class="fa fa-trash"></i>
 							删除
+						</button>
+						<button class="btn btn-primary btn-sm" v-if="temporaryMatterUuids.length"
+						        @click.stop.prevent="moveBatch($createElement)">
+							<i class="fa fa-share"></i>
+							移动
 						</button>
 
 						<span class="btn btn-primary btn-sm btn-file">
@@ -62,7 +70,7 @@
 <script>
   import MatterPanel from './widget/MatterPanel'
   import UploadMatterPanel from './widget/UploadMatterPanel'
-
+  import MoveBatchPanel from './widget/MoveBatchPanel'
   import NbSlidePanel from '../../common/widget/NbSlidePanel.vue'
   import NbExpanding from '../../common/widget/NbExpanding.vue'
   import NbCheckbox from '../../common/widget/NbCheckbox.vue'
@@ -80,6 +88,8 @@
         matter: new Matter(),
         //准备新建的文件。
         newMatter: new Matter(),
+	      //目标文件夹，用于移动操作
+        targetMatter: new Matter(),
         //准备上传的一系列文件
         uploadMatters: [],
         //临时暂存区，用于文件的相关操作
@@ -88,12 +98,14 @@
         user: this.$store.state.user,
         breadcrumbs: this.$store.state.breadcrumbs,
         director: new Director()
+
       }
     },
     components: {
-      NbCheckbox,
       MatterPanel,
       UploadMatterPanel,
+      MoveBatchPanel,
+      NbCheckbox,
       NbFilter,
       NbPager,
       NbSlidePanel,
@@ -125,8 +137,8 @@
 
         let that = this
 
-	      //清空暂存区
-        this.temporaryMatterUuids.splice(0,this.temporaryMatterUuids.length)
+        //清空暂存区
+        this.temporaryMatterUuids.splice(0, this.temporaryMatterUuids.length)
 
         let uuid = that.pager.getFilterValue('puuid')
 
@@ -240,8 +252,8 @@
         }
         return true
       },
-	    //批量删除
-      deleteBatch(){
+      //批量删除
+      deleteBatch () {
         let that = this
         MessageBox.confirm('此操作将永久删除这些文件, 是否继续?', '提示', {
           confirmButtonText: '确定',
@@ -250,7 +262,7 @@
           callback: function (action, instance) {
             if (action === 'confirm') {
               let uuids = that.temporaryMatterUuids.join(',')
-              that.matter.httpDeleteBatch(uuids,function (response) {
+              that.matter.httpDeleteBatch(uuids, function (response) {
                 Message.success('删除成功！')
                 that.refresh()
               })
@@ -258,7 +270,34 @@
 
           }
         })
+      },
+      //批量移动
+      moveBatch (createElement) {
+        let that = this
+	      let newMatter = new Matter()
+        let dom = createElement(MoveBatchPanel, {
+          props: {
+            targetMatter: this.targetMatter
+          }
+        })
 
+        MessageBox({
+          title: '移动到',
+          message: dom,
+          customClass: 'wp50',
+          confirmButtonText: '确定',
+          showCancelButton: true,
+          cancelButtonText: '关闭',
+          callback: (action, instance) => {
+            if (action === 'confirm') {
+              let uuids = that.temporaryMatterUuids.join(',')
+              that.matter.httpMove(uuids, this.targetMatter.uuid, function (response) {
+                Message.success('移动成功！')
+                that.refresh()
+              })
+            }
+          }
+        })
       }
     },
     watch: {
---
+++ src/backyard/matter/widget/FolderTree.vue
@@ -0,0 +1,100 @@
+<template>
+	<div class="folder-tree">
+		<div class="h50 cursor folder-block" :class="{'bg-silver-white': targetMatter.uuid === matter.uuid}" @click.stop.prevent="deepFolderToggle">
+			<span class="fa fa-chevron-down mr5" v-if="pager.data.length && deepFolder"></span>
+			<span class="fa fa-chevron-right mr5" v-if="pager.data.length && !deepFolder"></span>
+			<span :class="{ 'ml18': !pager.data.length }">
+				<span v-if="matter.uuid">
+					<img :src="matter.getIcon()" class="mr5" alt="文件夹" width="24" />
+					<span>{{matter.name}}</span>
+				</span>
+				<span v-else>
+					<span>全部文件</span>
+				</span>
+
+			</span>
+
+		</div>
+		<div v-if="pager.data.length && deepFolder" class="pl20">
+			<div v-for="(child, index) in pager.data">
+				<FolderTree :matter="child" :targetMatter="targetMatter"></FolderTree>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import NbExpanding from '../../../common/widget/NbExpanding'
+  import Pager from '../../../common/model/base/Pager'
+  import Matter from '../../../common/model/matter/Matter'
+
+  export default {
+    name: 'FolderTree',
+
+	  data(){
+      return{
+        deepFolder: false,
+        pager: new Pager(Matter)
+      }
+	  },
+	  props:{
+      targetMatter: {
+        type: Matter,
+	      required: true
+      },
+      matter: {
+        type: Matter,
+        required: true
+      },
+      deepFolderInit:{
+        type: Boolean,
+	      required: false,
+	      default: false
+      }
+	  },
+    watch: {
+
+    },
+	  methods:{
+      deepFolderToggle () {
+        this.deepFolder = !this.deepFolder
+	      if(this.targetMatter.uuid !== this.matter.uuid){
+          this.targetMatter.render(this.matter)
+	      }
+      }
+	  },
+	  components:{
+      NbExpanding
+
+	  },
+	  mounted() {
+      if (!this.matter.uuid) {
+        this.pager.setFilterValue('puuid', 'root')
+      } else {
+        this.pager.setFilterValue('puuid', this.matter.uuid)
+      }
+
+      if(this.deepFolderInit){
+        this.deepFolder = true
+      }
+
+      this.pager.setFilterValue('dir', true)
+      this.pager.httpFastPage()
+
+	  }
+
+  }
+</script>
+
+<style lang="less" rel="stylesheet/less">
+	.folder-tree{
+		.folder-block {
+			padding: 5px 10px;
+			line-height: 40px;
+			&:hover {
+				background: #eee;
+			}
+		}
+	}
+
+</style>
