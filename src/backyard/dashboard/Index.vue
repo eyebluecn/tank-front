65c32463b1d67d5d75711fff7d32593d74a888cc
zicla <ziclax@163.com>
2018-12-01 22:22:29 +0800

    Add the placeholder for dashboard.

--- src/backyard/dashboard/Index.vue
+++ src/backyard/dashboard/Index.vue
@@ -1,17 +1,122 @@
 <template>
   <div class="dashboard-index">
-    <div class="row">
-      <div class="col-md-6">
-        <div class="pedia-navigation">
-          <span class="item active">网站偏好</span>
-        </div>
-        <ECharts
-          ref="invokeListChart"
-          theme="ovilia-green"
-          auto-resize
-          :initOptions="initOptions"
-          :options="invokeListOption"/>
+    <div>
+      <div class="pedia-navigation">
+        <span class="item active">大盘监控</span>
       </div>
+    </div>
+    <div class="row mt20">
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="text-block">
+          <div class="upper">
+            <div class="indicator">总访问量</div>
+            <div class="amount">1265345</div>
+            <div>
+              <span class="rate">周同比 12% <i class="fa fa-caret-down text-success"></i></span>
+              <span class="rate">日同比 11% <i class="fa fa-caret-up text-danger"></i></span>
+            </div>
+          </div>
+          <div class="lower">
+            昨日访问量：13456
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="text-block">
+          <div class="upper">
+            <div class="indicator">总访问IP</div>
+            <div class="amount">1265345</div>
+            <div>
+              <span class="rate">周同比 12% <i class="fa fa-caret-down text-success"></i></span>
+              <span class="rate">日同比 11% <i class="fa fa-caret-up text-danger"></i></span>
+            </div>
+          </div>
+          <div class="lower">
+            昨日访IP：13456
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="text-block">
+          <div class="upper">
+            <div class="indicator">文件总数</div>
+            <div class="amount">1265345</div>
+            <div>
+              <span class="rate">周同比 12% <i class="fa fa-caret-down text-success"></i></span>
+              <span class="rate">日同比 11% <i class="fa fa-caret-up text-danger"></i></span>
+            </div>
+          </div>
+          <div class="lower">
+            昨日新增文件数：13456
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="text-block">
+          <div class="upper">
+            <div class="indicator">文件总大小(包括缓存)</div>
+            <div class="amount">1265345 G</div>
+            <div>
+              <span class="rate">周同比 12% <i class="fa fa-caret-down text-success"></i></span>
+              <span class="rate">日同比 11% <i class="fa fa-caret-up text-danger"></i></span>
+            </div>
+          </div>
+          <div class="lower">
+            昨日新增文件数：13456
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="figure-block">
+          <div class="title">
+            最近15日调用量/UV
+          </div>
+          <figure>
+            <ECharts
+              ref="invokeListChart"
+              theme="ovilia-green"
+              :autoResize="true"
+              :initOptions="initOptions"
+              :options="invokeListOption"/>
+          </figure>
+        </div>
+      </div>
+
+      <div class="col-lg-6 col-md-6 col-sm-12">
+        <div class="figure-block">
+          <div class="title">
+            文件下载量TOP10
+          </div>
+          <figure>
+            <ECharts
+              ref="invokeListChart"
+              theme="ovilia-green"
+              :autoResize="true"
+              :initOptions="initOptions"
+              :options="invokeListOption"/>
+          </figure>
+        </div>
+
+      </div>
+
+      <div class="col-lg-6 col-md-6 col-sm-12">
+        <div class="figure-block">
+          <div class="title">
+            活跃用户TOP10
+          </div>
+          <figure>
+            <ECharts
+              ref="invokeListChart"
+              theme="ovilia-green"
+              :autoResize="true"
+              :initOptions="initOptions"
+              :options="invokeListOption"/>
+          </figure>
+        </div>
+
+      </div>
+
+
     </div>
 
   </div>
@@ -68,7 +173,7 @@
           },
           xAxis: {
             name: "日期",
-            data: [1, 2, 3, 4, 5, 6, 7]
+            data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
           },
           yAxis: {
             name: "数量"
@@ -76,11 +181,11 @@
           series: [{
             name: '调用量',
             type: 'bar',
-            data: [1, 2, 3, 4, 5, 6, 7]
+            data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
           }, {
             name: 'UV',
             type: 'line',
-            data: [1, 2, 3, 4, 5, 6, 7]
+            data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
           }]
         }
       }
@@ -109,9 +214,8 @@
       }
     },
     mounted() {
-
+      let that = this;
       this.refreshInvokeList()
-
     }
   }
 
@@ -121,6 +225,67 @@
   @import "../../assets/css/global/variables";
 
   .dashboard-index {
+
+    figure {
+      .echarts {
+        width: 100%;
+        height: 300px;
+      }
+    }
+
+    .text-block {
+      background-color: white;
+      box-shadow: 0 0 5px rgba(0, 0, 0, .2);
+      border-radius: 5px;
+      padding: 20px 15px 10px 15px;
+      margin-bottom: 30px;
+      .upper {
+
+        .indicator {
+          color: rgba(0, 0, 0, .45);
+          font-size: 14px;
+          line-height: 22px;
+          height: 22px;
+        }
+        .amount {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          word-break: break-all;
+          white-space: nowrap;
+          color: rgba(0, 0, 0, .85);
+          margin-top: 4px;
+          margin-bottom: 20px;
+          font-size: 30px;
+          line-height: 38px;
+          height: 38px;
+
+        }
+        .rate {
+          margin-right: 15px;
+        }
+      }
+      .lower {
+        margin-top: 10px;
+        padding-top: 10px;
+        border-top: 1px solid #eee;
+        font-size: 14px;
+      }
+    }
+    .figure-block {
+      background-color: white;
+      box-shadow: 0 0 5px rgba(0, 0, 0, .2);
+      border-radius: 5px;
+      margin-bottom: 20px;
+
+      .title {
+        font-size: 18px;
+        padding: 15px 20px;
+        color: black;
+        margin-bottom: 10px;
+        border-bottom: 1px solid #eee;
+      }
+
+    }
 
   }
 </style>
