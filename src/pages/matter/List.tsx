36a890ceb4250b338cdd177b0401f20c4069ee18
lishuang <ziclax@163.com>
2020-06-06 00:24:22 +0800

    Add the feature.

--- src/pages/matter/List.tsx
+++ src/pages/matter/List.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link, RouteComponentProps } from 'react-router-dom';
+import {Link, RouteComponentProps} from 'react-router-dom';
 import './List.less';
 import TankComponent from '../../common/component/TankComponent';
 import SortDirection from '../../common/model/base/SortDirection';
 import Pager from '../../common/model/base/Pager';
 import Matter from '../../common/model/matter/Matter';
-import Table, { ColumnProps } from 'antd/lib/table';
+import Table, {ColumnProps} from 'antd/lib/table';
 import StringUtil from '../../common/util/StringUtil';
-import { Button, message as MessageBox, Popconfirm } from 'antd';
+import {Button, message as MessageBox, Popconfirm} from 'antd';
 import DateUtil from '../../common/util/DateUtil';
 import FilterPanel from '../widget/filter/FilterPanel';
 import TableEmpty from '../widget/TableEmpty';
@@ -62,7 +62,13 @@ export default class List extends TankComponent<IProps, IState> {
       that.pager.setFilterValue('orderCreateTime', SortDirection.DESC);
     }
 
-    that.pager.httpList();
+    that.updateUI()
+    that.pager.httpList(function () {
+      that.updateUI()
+    }, null, function () {
+      that.updateUI()
+    });
+
   }
 
   createMatter() {
@@ -131,7 +137,7 @@ export default class List extends TankComponent<IProps, IState> {
           </Link>
 
           <Popconfirm title="确认删除该文件，删除后不可恢复?" onConfirm={(e: any) => {
-            record.httpDel(function() {
+            record.httpDel(function () {
               MessageBox.success('删除成功！');
               that.refresh();
             });
@@ -162,7 +168,7 @@ export default class List extends TankComponent<IProps, IState> {
           columns={columns}
           pagination={pager.getPagination()}
           onChange={pager.tableOnChange.bind(pager)}
-          locale={{ emptyText: (<TableEmpty pager={pager} onRefresh={this.refresh.bind(this)}/>) }}
+          locale={{emptyText: (<TableEmpty pager={pager} onRefresh={this.refresh.bind(this)}/>)}}
         />
       </div>
     );
