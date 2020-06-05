7332a1807d73c0c3b6039274085112fbf3315c06
lishuang <ziclax@163.com>
2020-06-05 23:26:14 +0800

    add the bamboo demo

--- config/paths.js
+++ config/paths.js
@@ -62,7 +62,7 @@ module.exports = {
   appJsConfig: resolveApp('jsconfig.json'),
   yarnLockFile: resolveApp('yarn.lock'),
   testsSetup: resolveModule(resolveApp, 'src/setupTests'),
-  proxySetup: resolveApp('src/setupProxy.js'),
+  proxySetup: resolveApp('config/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
   publicUrlOrPath,
 };
