666783e11ee103af1656d27610bb41ee1cdb07bb
Simba <ycyin8795@163.com>
2021-01-02 23:10:53 +0800

    upload cancel.

--- src/pages/matter/widget/UploadMatterPanel.tsx
+++ src/pages/matter/widget/UploadMatterPanel.tsx
@@ -3,9 +3,9 @@ import TankComponent from "../../../common/component/TankComponent";
 import Matter from "../../../common/model/matter/Matter";
 import StringUtil from "../../../common/util/StringUtil";
 import { Progress } from "antd";
+import { CloseOutlined } from "@ant-design/icons";
 import "./UploadMatterPanel.less";
 import Lang from "../../../common/model/global/Lang";
-import {UserRole} from "../../../common/model/user/UserRole";
 
 interface IProps {
   matter: Matter;
@@ -23,19 +23,13 @@ export default class UploadMatterPanel extends TankComponent<IProps, IState> {
     return `${percent.toFixed(1)}%`;
   };
 
-
-
   componentDidMount() {
-
-    this.props.matter.reactComponent = this
+    this.props.matter.reactComponent = this;
   }
 
-  componentWillReceiveProps(nextProps: Readonly<IProps>, nextContext: any) {
-
-
-  }
+  componentWillReceiveProps(nextProps: Readonly<IProps>, nextContext: any) {}
   componentWillUnmount() {
-    this.props.matter.reactComponent = null
+    this.props.matter.reactComponent = null;
   }
 
   render() {
@@ -46,6 +40,9 @@ export default class UploadMatterPanel extends TankComponent<IProps, IState> {
           <div className="huge-block clearfix">
             <div className="media">
               <div className="media-body">{matter.file!.name}</div>
+              <div className="media-cancel">
+                <CloseOutlined onClick={() => matter.cancelUpload()} />
+              </div>
             </div>
             <Progress
               className="progress"
