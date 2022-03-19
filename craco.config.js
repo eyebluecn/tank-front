42e9c5ad2e6d5bf38ed1433773e5fae23dfcda22
simba <ycyin8795@163.com>
2022-03-19 20:24:40 +0800

    fix: craco less warning

--- craco.config.js
+++ craco.config.js
@@ -20,6 +20,11 @@ module.exports = {
       // less loader.
       plugin: CracoLessPlugin,
       options: {
+        // resolve-url-loader只对sass生效，craco-less默认使用sass配置，所以这里手动过滤掉resolve-url-loader
+        modifyLessRule: (lessRule) => {
+          lessRule.use = lessRule.use.filter(i => !i.loader.includes('resolve-url-loader'));
+          return lessRule;
+        },
         lessLoaderOptions: {
           lessOptions: {
             modifyVars: {'@primary-color': '#215891'},
