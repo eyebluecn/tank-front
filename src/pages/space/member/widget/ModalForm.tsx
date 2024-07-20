bc7737cd0ed7d3c857569289377f3ab6266a93ff
yinyuchen <ycyin8795@163.com>
2024-07-20 15:25:06 +0800

    feat: space member batch create

--- src/pages/space/member/widget/ModalForm.tsx
+++ src/pages/space/member/widget/ModalForm.tsx
@@ -2,22 +2,37 @@ import Lang from '../../../../common/model/global/Lang';
 import { Form, Input, Modal, Select } from 'antd';
 import React, { useEffect, useRef, useState } from 'react';
 import { FORM_LAYOUT } from '../../../../common/const/Form';
-import { SpaceMemberFormValues } from '../../../../common/model/space/member/SpaceMember';
-import { SpaceMemberRoleList } from '../../../../common/model/space/member/SpaceMemberRole';
+import SpaceMember from '../../../../common/model/space/member/SpaceMember';
+import {
+  SpaceMemberRole,
+  SpaceMemberRoleList,
+} from '../../../../common/model/space/member/SpaceMemberRole';
 import Pager from '../../../../common/model/base/Pager';
 import User from '../../../../common/model/user/User';
 
+export interface SpaceMemberModalFormValues {
+  userUuids: string[];
+  role: SpaceMemberRole;
+}
+
 interface Props {
   mode: 'create' | 'edit';
-  initialValues?: SpaceMemberFormValues;
-  onOk: (values: SpaceMemberFormValues) => void;
+  targetSpaceMember?: SpaceMember;
+  onOk: (values: SpaceMemberModalFormValues) => void;
   onCancel: () => void;
 }
-const ModalForm = ({ mode, initialValues, onOk, onCancel }: Props) => {
+const ModalForm = ({ mode, targetSpaceMember, onOk, onCancel }: Props) => {
   const isCreate = mode === 'create';
   const userPager = useRef(new Pager<User>(null, User, 100));
-  const [form] = Form.useForm<SpaceMemberFormValues>();
+  const [form] = Form.useForm<SpaceMemberModalFormValues>();
   const [userList, setUserList] = useState<User[]>([]); // 用户列表
+  const initialValues: SpaceMemberModalFormValues | undefined =
+    targetSpaceMember
+      ? {
+          userUuids: [targetSpaceMember.userUuid!],
+          role: targetSpaceMember.role!,
+        }
+      : undefined;
 
   useEffect(() => {
     refresh();
@@ -49,12 +64,8 @@ const ModalForm = ({ mode, initialValues, onOk, onCancel }: Props) => {
         onFinish={onOk}
         initialValues={initialValues}
       >
-        {/*编辑时暂存uuid*/}
-        <Form.Item name="uuid" hidden>
-          <Input />
-        </Form.Item>
         <Form.Item
-          name="userUuid"
+          name="userUuids"
           label={Lang.t('space.member')}
           rules={[{ required: true, message: Lang.t('selectRequired') }]}
         >
@@ -63,6 +74,7 @@ const ModalForm = ({ mode, initialValues, onOk, onCancel }: Props) => {
               label: item.username,
               value: item.uuid,
             }))}
+            mode="multiple"
             placeholder={Lang.t('space.memberTip')}
             disabled={!isCreate}
             showSearch
